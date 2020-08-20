816a2114bf790e1115f031f5f8151c47eab75985
Andrei Kislichenko <android.2net@gmail.com>
2020-08-20 18:59:07 +0200

    fix test

--- spec/relaton_ogc/ogc_bibliographic_item_spec.rb
+++ spec/relaton_ogc/ogc_bibliographic_item_spec.rb
@@ -13,6 +13,5 @@ RSpec.describe RelatonOgc::OgcBibliographicItem do
     file = "spec/fixtures/asciibib.adoc"
     File.write file, bib, encoding: "UTF-8" unless File.exist? file
     expect(bib).to eq File.read(file, encoding: "UTF-8")
-      .gsub(/(?<=fetched::\s)\d{4}-\d{2}-\d{2/, Date.today.to_s)
   end
 end
